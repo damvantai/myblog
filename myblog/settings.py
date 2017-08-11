d01366997adb5bd96812edb48ed51daf9cf850c2
damvantai <tai94bn@gmail.com>
2017-08-11 21:55:21 +0700

    all css blog.css and send email

--- myblog/settings.py
+++ myblog/settings.py
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'tai94bn@gmail.com'
+EMAIL_HOST_PASSWORD = 'relax1994'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
