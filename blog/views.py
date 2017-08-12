107da31246f58b549aab01eb9739d04e752a3cfc
damvantai <tai94bn@gmail.com>
2017-08-12 09:41:43 +0700

    position of tag in list.html

--- blog/views.py
+++ blog/views.py
@@ -17,6 +17,7 @@ def post_list(request, tag_slug = None):
     if tag_slug:
         tag = get_object_or_404(Tag, slug=tag_slug)
         object_list = object_list.filter(tags__in = [tag])
+
     paginator = Paginator(object_list, 3)
     page = request.GET.get('page')
     try:
@@ -26,7 +27,7 @@ def post_list(request, tag_slug = None):
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html',{'page': page,
-        'posts': posts})
+        'posts': posts, 'tag': tag})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug = post,
