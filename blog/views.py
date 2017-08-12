a4ad065698ebd93e18ed27382bd1ab36c2bcedcc
damvantai <tai94bn@gmail.com>
2017-08-12 09:08:57 +0700

    change postlistview - listview (

--- blog/views.py
+++ blog/views.py
@@ -5,19 +5,28 @@ from django.views.generic import ListView
 
 from .models import Post
 from .forms import EmailPostForm
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
+from taggit.models import Tag
+
 # Create your views here.
-# def post_list(request):
-#     object_list = Post.published.all()
-#     paginator = Paginator(object_list, 3)
-#     page = request.GET.get('page')
-#     try:
-#         posts = paginator.page(page)
-#     except PageNotAnInteger:
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         posts = paginator.page(paginator.num_pages)
-#     return render(request, 'blog/post/list.html',{'page': page,
-#         'posts': posts})
+def post_list(request, tag_slug = None):
+    object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in = [tag])
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html',{'page': page,
+        'posts': posts})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug = post,
@@ -25,13 +34,28 @@ def post_detail(request, year, month, day, post):
                              publish__year = year,
                              publish__month = month,
                              publish__day = day)
-    return render(request, 'blog/post/detail.html',{'post': post})
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+            
+    return render(request, 'blog/post/detail.html',{'post': post, 'comments':comments, 'comment_form': comment_form})
+
+# class PostListView(ListView):
+#     queryset = Post.published.all()
+#     context_object_name = 'posts'
+#     paginate_by = 3
+#     template_name = 'blog/post/list.html'
 
 def post_share(request, post_id):
     # Retrieve post by id
